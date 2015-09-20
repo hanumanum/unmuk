3ae66d4f316adfddbe13771a575e046f8374ebf1
hanumanum <a.kolyan@gratun.am>
2015-09-20 20:23:35 +0400

    Համարյա սկիզբ

---
+++ js/common.js
@@ -0,0 +1,9 @@
+        document.captureEvents(Event.MOUSEDOWN | Event.MOUSEUP);
+        document.onmousedown = clickIE4;
+        document.onmouseup = clickIE4;
+
+        function clickIE4()
+        {
+            //alert("ահա՜, բռնվեցի՞ր , ստեղաշարով արա")
+            //return false; 
+        }
