b959c622995d189ef341761e68f2d43d844cf005
hanumanum <a.kolyan@gratun.am>
2015-12-22 19:39:36 +0400

    fork me

--- template/footer.php
+++ template/footer.php
@@ -1,3 +1,4 @@
+<a href="https://github.com/hanumanum/unmuk"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
 <div class="container-fluid">
         <!--
         <ul class="list-group list-inline">
@@ -110,22 +111,6 @@
   })();
 </script>            
 <!-- Google Analitycs end -->
-
-<!-- facebook like -->
-<script>
-/*
-(function(d, s, id) {
-
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-*/
-</script>
-<!-- end of facebook like -->
-
 
 <script type="text/javascript">
   
